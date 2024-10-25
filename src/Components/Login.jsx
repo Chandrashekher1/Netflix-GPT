569dc88da4f1c70a60f90df2d565efdc32c3b19e
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-10-25 22:58:08 +0530

    Add login page with auth

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValiData } from "../utils/validate";
 
 const Login = () => {
     const [isSignInForm, setIsSignInForm] = useState(true); 
+
+    const [errorMessage,setErrorMessage] = useState(null)
+
+    const email = useRef(null)
+    const password = useRef(null)
+
+    const handleLoginValidate = () => {
+        const message = checkValiData(email.current.value, password.current.value)
+        
+        setErrorMessage(message)
+        
+    }
 
     const toggleSignInForm = (event) => {
         event.preventDefault(); 
@@ -17,10 +30,10 @@ const Login = () => {
                     src="https://assets.nflxext.com/ffe/siteui/vlv3/74d734ca-0eab-4cd9-871f-bca01823d872/web/IN-en-20241021-TRIFECTA-perspective_2277eb50-9da3-4fdf-adbe-74db0e9ee2cf_large.jpg"
                     alt="bg-image"
                     className="bg-slate-900"
-                />
+                /> 
             </div>
 
-            <form action="" className="absolute p-10 w-1/4 h-auto my-36 mx-auto left-0 right-0 bg-black bg-opacity-80">
+            <form onSubmit={(e) => e.preventDefault()} action="" className="absolute p-10 w-1/4 h-auto my-36 mx-auto left-0 right-0 bg-black bg-opacity-80">
                 <h1 className="font-bold text-3xl py-4 text-white">
                     {isSignInForm ? "Sign In" : "Sign Up"}
                 </h1>
@@ -34,17 +47,22 @@ const Login = () => {
                     placeholder="sureName"
                     className="py-2 my-4 bg-gray-700 w-full rounded-md p-2"
                 />}
-                <input
+                <input 
+                    ref={email}
                     type="text"
-                    placeholder="Email or mobile number"
+                    placeholder="Email or mobile number" 
                     className="py-2 my-4 bg-gray-700 w-full rounded-md p-2"
                 />
                 <input
+                    ref={password}
                     type="password"
                     placeholder="Password"
                     className="py-2 bg-gray-700 my-4 w-full rounded-md p-2"
                 />
-                <button className="py-4 my-4 bg-red-700 w-full rounded-md">
+
+                <p className="text-red-800 font-semibold text-xl py-2" >{errorMessage}</p>
+
+                <button className="py-4 my-4 bg-red-700 w-full rounded-md" onClick={handleLoginValidate}>
                     {isSignInForm ? "Sign In" : "Sign Up"}
                 </button>
                 <p className="text-gray-500 py-4">
